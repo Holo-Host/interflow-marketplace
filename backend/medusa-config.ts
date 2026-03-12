2ba7d2dfde5137cb2560adca574d9f7c8c0df155
Robert Lyon <sir.rob@holo.host>
2026-03-12 10:17:59 -0300

    remove resend module from medusa config

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -65,21 +65,5 @@ module.exports = defineConfig({
         redisUrl: process.env.REDIS_URL 
       },
     },
-    {
-      resolve: "@medusajs/medusa/notification",
-      options: {
-        providers: [
-          {
-            resolve: "./src/modules/resend",
-            id: "resend",
-            options: {
-              channels: ["email"],
-              api_key: process.env.RESEND_API_KEY,
-              from: process.env.RESEND_FROM_EMAIL,
-            },
-          },
-        ],
-      },
-    },
   ]
 })
